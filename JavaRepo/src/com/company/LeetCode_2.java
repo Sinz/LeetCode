824fe6cf6177c27a2784587126315ebedad76a12
zhaojie <809733865@qq.com>
2020-08-15 14:44:55 +0800

    leetcode[2]

--- JavaRepo/src/com/company/LeetCode_2.java
+++ JavaRepo/src/com/company/LeetCode_2.java
@@ -21,7 +21,52 @@ package com.company;
 public class LeetCode_2 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-    
+        // 1. 判断是否为0
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+
+        // 2. 当l1 和 l2都存在时
+        int carry = 0 ;
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while(l1 != null && l2 !=null){
+            int sum = l1.val + l2.val + carry;
+            int val = sum%10;
+            carry = sum/10;
+
+            ListNode newNode = new ListNode(val);
+            current.next = newNode;
+            current = current.next;
+
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        // 3. l1 或 l2 不存在时
+        while(l1 != null){
+            int sum = l1.val + carry;
+            int val = sum%10;
+            carry = sum/10;
+            ListNode newNode = new ListNode(val);
+            current.next = newNode;
+            current = current.next;
+            l1 = l1.next;
+        }
+        while(l2 != null){
+            int sum = l2.val + carry;
+            int val = sum%10;
+            carry = sum/10;
+            ListNode newNode = new ListNode(val);
+            current.next = newNode;
+            current = current.next;
+            l2 = l2.next;
+        }
+
+        // 4. 当 l1 和 l2 都不存在时，判断carry是否为0
+        if(carry != 0) current.next = new ListNode(carry);
+        return dummy.next;
+    }
 
     public class ListNode {
         int val;
