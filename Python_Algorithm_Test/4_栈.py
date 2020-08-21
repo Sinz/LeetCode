f4c4d0ddb0fc36e604081743fdad17094655fcc2
zhaojie <809733865@qq.com>
2020-08-21 19:29:49 +0800

    队列

---
+++ Python_Algorithm_Test/4_栈.py
@@ -0,0 +1,30 @@
+class Stack(object):
+    def __init__(self):
+        self.items = []
+    
+    def is_empty(self):
+        return self.items == []
+
+    def push(self,item): # 加入元素
+        self.items.append(item)
+
+    def pop(self): # 弹出元素
+        return self.items.pop()
+
+    def peek(self): # 返回栈顶元素
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
+    
+if __name__ == "__main__":
+    stack = Stack()
+    stack.push("hello")
+    stack.push("world")
+    stack.push("itcast")
+    print (stack.size())
+    print (stack.peek())
+    print (stack.pop())
+    print (stack.pop())
+    print (stack.pop())
