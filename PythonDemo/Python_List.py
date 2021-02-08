72c49221879941db96836e06fae783cca8c7fe68
zhaojie <809733865@qq.com>
2021-02-08 16:44:09 +0800

    add

--- PythonDemo/Python_List.py
+++
@@ -1,8 +0,0 @@
- 
-if __name__ == '__main__':
-     
-    list1 = ['physics', 'chemistry', 1997, 2000]
-    list2 = [1, 2, 3, 4, 5, 6, 7 ]
-    
-    print ("list1[0]: ", list1[0])
-    print ("list2[1:5]: ", list2[0:5]) # 截取时包括左 不包括右
