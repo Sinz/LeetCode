c6a2a5ea9538fc529ac9bcd2206af30cfad7802d
zhaojie <809733865@qq.com>
2021-03-24 13:59:09 +0800

    photp

---
+++ JavaRepo/src/com/leetcode/LeetCode_94_BinaryTreeInorderTraversal.java
@@ -0,0 +1,96 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+//给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [1,null,2,3]
+//输出：[1,3,2]
+//
+//
+// 示例 2：
+//
+//
+//输入：root = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：root = [1]
+//输出：[1]
+//
+//
+// 示例 4：
+//
+//
+//输入：root = [1,2]
+//输出：[2,1]
+//
+//
+// 示例 5：
+//
+//
+//输入：root = [1,null,2]
+//输出：[1,2]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数目在范围 [0, 100] 内
+// -100 <= Node.val <= 100
+//
+//
+//
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+// Related Topics 栈 树 哈希表
+// 👍 872 👎 0
+
+
+public class LeetCode_94_BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        //中序遍历，那就按照 左-打印-右这种顺序遍历树就可以了，递归函数实现
+        if ( root == null ){
+            return null;
+        }
+        List<Integer> res = new ArrayList<Integer>();
+        dfs(res, root);
+        return res;
+    }
+
+    public void dfs(List<Integer> res, TreeNode root){
+
+        if(root==null) {
+            return;
+        }
+        //按照 左-打印-右的方式遍历
+        dfs(res,root.left);
+        res.add(root.val);
+        dfs(res,root.right);
+    }
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){}
+        TreeNode(int val){
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
